2d52239919720b5bac58f1740ec9971543cf1fe2
don1x <mityafotografer@gmail.com>
2025-12-14 20:34:32 +0300

    Added paper main project

---
+++ src/main/java/ru/yuuki/swiftparticles/SwiftParticles.java
@@ -0,0 +1,17 @@
+package ru.yuuki.swiftparticles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SwiftParticles extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
